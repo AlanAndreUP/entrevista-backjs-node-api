5febf6c5154a62e34d7737d5ee4bd21b078e5fea
AlanAndreUp <cosaspami7@gmail.com>
2024-10-03 12:54:09 -0600

    hotfix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,17 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from '../utils/auth';
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
   const token = req.headers['authorization']?.split(' ')[1];
 
   if (!token) {
-    return res.sendStatus(401); 
+    res.sendStatus(401);
+    return;
   }
 
   try {
     const user = verifyToken(token); 
+    req.user = user;
     next();
   } catch (error) {
-    return res.sendStatus(403);
+    res.sendStatus(403);
   }
 };
--- src/routes/client.route.ts
+++ src/routes/client.route.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { createCustomerController, getCustomersController } from '../controllers/client.controller';
-
 const router = Router();
 
 router.post('/customers', createCustomerController);
